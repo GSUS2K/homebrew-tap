dc770395d49c2fc93bd5310a7d3272b0823736b2
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-04-05 14:32:41 +0530

    Update Aether cask version and URLs

--- Casks/aether.rb
+++ Casks/aether.rb
@@ -1,13 +1,13 @@
 cask "aether" do
-  version "2026.04.05.alpha2"
+  version "2026.04.05.alphabfig"
   on_arm do
-    sha256 "6988c9153bddce13575ae14b623ee18a2040fdda6d8efb500628d260c418f931"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.05.alpha2/Aether-12.11.1-SOVEREIGN-arm64.dmg"
+    sha256 "7ec01f619d7e66c4aedfd6de269c496ed2194dcc983bec7c9f6a9744d37f63a8"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.05.alphabfig/Aether-12.11.1-SOVEREIGN-arm64.dmg"
   end
 
   on_intel do
-    sha256 "9c31ff9d1fdc261babee0b7e534980db651d90fc252424372b37cda7203b3a6f"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.05.alpha2/Aether-12.11.1-SOVEREIGN.dmg"
+    sha256 "8778ddd4c94e6c013b5479e87e09286080bec37fdc5fb8fcd90127c3252a2959"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.05.alphabfig/Aether-12.11.1-SOVEREIGN.dmg"
   end
 
   name "Aether"
