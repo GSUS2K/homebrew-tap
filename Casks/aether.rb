c24eba8316dde90a35603c2a76baa587f839a33d
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-04-06 18:52:44 +0530

    Update Aether version and download URLs

--- Casks/aether.rb
+++ Casks/aether.rb
@@ -1,13 +1,13 @@
 cask "aether" do
-  version "2026.04.06.2"
+  version "2026.04.06.3"
   on_arm do
-    sha256 "1fed32e9fab3bd6a1a0995ad8c8688afd553c7ef86f27de909cd35742e7657db"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.06.2/Aether-Utario-v2-macOS-arm64.dmg"
+    sha256 "ddc9cbe0679656dcfce7697adbb283d2e076970a12369eff710da4c4e424e463"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.06.3/Aether-2026.4.6-arm64.dmg"
   end
 
   on_intel do
-    sha256 "57a1e810bfeed2671008e62a95ff0300b6a80a7ba9f89127d7b03e9a66f643de"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.06.2/Aether-Utario-v2-macOS-intelx64.dmg"
+    sha256 "75b126ad4859166dd9a18619d55842a7386365f3e262bbcbbf84993555faeaae"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.04.06.3/Aether-2026.4.6.dmg"
   end
 
   name "Aether"
