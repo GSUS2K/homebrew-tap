4d0e8394e84e8aac2736f8f3dca420e8757603ce
GSUS2K <100770858+GSUS2K@users.noreply.github.com>
2026-04-22 02:29:10 +0530

    Update Aether version to 2026.4.26

--- Casks/aether.rb
+++ Casks/aether.rb
@@ -1,13 +1,13 @@
 cask "aether" do
-  version "2026.4.23"
+  version "2026.4.26"
   on_arm do
-    sha256 "d3f651c5bc60117880cf835e955edf362d231459f15af7954cade6367aa31a9c"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.4.23/Aether-arm64.dmg"
+    sha256 "47c3609012c73603023c112fc57b860f28df7abaf2e8c37ca5b7b721197f7a21"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.4.26/Aether-arm64.dmg"
   end
 
   on_intel do
-    sha256 "f89b7ec059972360855339e6e7f4beea3bf9bb2933dbef61f976a3360be47afc"
-    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.4.23/Aether-x64.dmg"
+    sha256 "4bdc5125e30671800ec8d27d32c215da84ac5a19d9a4d9d17f59fa67b44a8de7"
+    url "https://github.com/GSUS2K/Aether-Studio/releases/download/v2026.4.26/Aether-x64.dmg"
   end
 
   name "Aether"
